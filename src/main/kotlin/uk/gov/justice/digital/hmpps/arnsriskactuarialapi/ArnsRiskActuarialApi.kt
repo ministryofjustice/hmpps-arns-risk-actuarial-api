94761d851507499e46774bf488d10387f1329e3f
runner <runner@hmpps-github-actions-runner-ff97cb54-5bm84>
2025-07-04 15:51:50 +0000

    Rename project

--- src/main/kotlin/uk/gov/justice/digital/hmpps/arnsriskactuarialapi/ArnsRiskActuarialApi.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/arnsriskactuarialapi/ArnsRiskActuarialApi.kt
@@ -1,11 +1,11 @@
-package uk.gov.justice.digital.hmpps.templatepackagename
+package uk.gov.justice.digital.hmpps.arnsriskactuarialapi
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
-class HmppsTemplateKotlin
+class ArnsRiskActuarialApi
 
 fun main(args: Array<String>) {
-  runApplication<HmppsTemplateKotlin>(*args)
+  runApplication<ArnsRiskActuarialApi>(*args)
 }
