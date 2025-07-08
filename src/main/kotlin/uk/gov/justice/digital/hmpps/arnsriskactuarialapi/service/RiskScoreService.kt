54fd6fbf6d9dff61be49efd5784497a22a10b0ed
“OliverYeoman” <Ollie.Yeoman@justice.gov.uk>
2025-07-08 16:04:06 +0100

    APP-119 risk score service setup

---
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/arnsriskactuarialapi/service/RiskScoreService.kt
@@ -0,0 +1,12 @@
+package uk.gov.justice.digital.hmpps.arnsriskactuarialapi.service
+
+import org.springframework.stereotype.Service
+
+@Service
+class RiskScoreService {
+
+  fun riskScoreProducer(riskScore: Int): Int {
+    //todo
+    return riskScore
+  }
+}
