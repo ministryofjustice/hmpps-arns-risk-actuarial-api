d2c02e331cfcb625207809a6fb02fc69ee8278dd
riccardo-noviello-moj-2 <Riccardo.Noviello@justice.gov.uk>
2025-07-15 12:45:15 +0100

    ACT-3: refactor agegroup calculation

--- src/main/kotlin/uk/gov/justice/digital/hmpps/arnsriskactuarialapi/service/transformation/Ogrs3TransformationHelper.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/arnsriskactuarialapi/service/transformation/Ogrs3TransformationHelper.kt
@@ -1,6 +1,5 @@
 package uk.gov.justice.digital.hmpps.arnsriskactuarialapi.service.transformation
 
-import uk.gov.justice.digital.hmpps.arnsriskactuarialapi.dto.AgeGroup
 import uk.gov.justice.digital.hmpps.arnsriskactuarialapi.dto.Gender
 import uk.gov.justice.digital.hmpps.arnsriskactuarialapi.dto.OffenderConvictionStatus
 import uk.gov.justice.digital.hmpps.arnsriskactuarialapi.dto.RiskBand
@@ -21,7 +20,6 @@ fun getAgeAtCurrentConviction(
   dateOfCurrentConviction: LocalDate,
   ageAtFirstSanction: Int,
 ): Result<Int> = runCatching {
-
   if (dateOfCurrentConviction.isBefore(dateOfBirth)) {
     throw IllegalArgumentException("Conviction date cannot be before date of birth.")
   }
@@ -41,8 +39,6 @@ fun getAgeAtCurrentConviction(
 
 fun getAgeAtStartOfFollowup(dateOfBirth: LocalDate, dateAtStartOfFollowup: LocalDate): Int = ChronoUnit.YEARS.between(dateOfBirth, dateAtStartOfFollowup).toInt()
 
-fun getOffenderAgeGroup(ageAtStartOfFollowup: Int) = getAgeGroup(ageAtStartOfFollowup)
-
 fun getNumberOfPreviousSanctions(totalNumberOfSanctions: Int) = totalNumberOfSanctions.minus(1)
 
 fun getOffenderConvictionStatus(totalNumberOfSanctions: Int) = if (totalNumberOfSanctions == 1) OffenderConvictionStatus.FIRST_TIME_OFFENDER else OffenderConvictionStatus.REPEAT_OFFENDER
@@ -61,40 +57,6 @@ fun getOffenderCopasScore(
   val logValue = ln(numerator / denominator)
 
   return BigDecimal(logValue, MathContext.DECIMAL64).toCommonScale()
-}
-
-fun getAgeGenderParameter(ageGroup: AgeGroup, gender: Gender): Double {
-  if (gender == Gender.MALE) {
-    return when (ageGroup) {
-      AgeGroup.TEN_TO_UNDER_TWELVE -> 0.0
-      AgeGroup.TWELVE_TO_UNDER_FOURTEEN -> 0.08392
-      AgeGroup.FOURTEEN_TO_UNDER_SIXTEEN -> 0.07578
-      AgeGroup.SIXTEEN_TO_UNDER_EIGHTEEN -> -0.0616
-      AgeGroup.EIGHTEEN_TO_UNDER_TWENTY_ONE -> -0.6251
-      AgeGroup.TWENTY_ONE_TO_UNDER_TWENTY_FIVE -> -1.0515
-      AgeGroup.TWENTY_FIVE_TO_UNDER_THIRTY -> -1.1667
-      AgeGroup.THIRTY_TO_UNDER_THIRTY_FIVE -> -1.326
-      AgeGroup.THIRTY_FIVE_TO_UNDER_FORTY -> -1.368
-      AgeGroup.FORTY_TO_UNDER_FIFTY -> -1.4997
-      AgeGroup.FIFTY_AND_OVER -> -2.0253
-    }
-  } else if (gender == Gender.FEMALE) {
-    return when (ageGroup) {
-      AgeGroup.TEN_TO_UNDER_TWELVE -> 0.785
-      AgeGroup.TWELVE_TO_UNDER_FOURTEEN -> 0.61385
-      AgeGroup.FOURTEEN_TO_UNDER_SIXTEEN -> 0.66952
-      AgeGroup.SIXTEEN_TO_UNDER_EIGHTEEN -> -0.9592
-      AgeGroup.EIGHTEEN_TO_UNDER_TWENTY_ONE -> -0.8975
-      AgeGroup.TWENTY_ONE_TO_UNDER_TWENTY_FIVE -> -1.0285
-      AgeGroup.TWENTY_FIVE_TO_UNDER_THIRTY -> -1.0528
-      AgeGroup.THIRTY_TO_UNDER_THIRTY_FIVE -> -1.1291
-      AgeGroup.THIRTY_FIVE_TO_UNDER_FORTY -> -1.4219
-      AgeGroup.FORTY_TO_UNDER_FIFTY -> -1.5247
-      AgeGroup.FIFTY_AND_OVER -> -2.4498
-    }
-  } else {
-    throw IllegalArgumentException("Unhandled gender: $gender")
-  }
 }
 
 fun getConvictionStatusParameter(offenderConvictionStatus: OffenderConvictionStatus) = if (offenderConvictionStatus == OffenderConvictionStatus.REPEAT_OFFENDER) 0.46306 else 0.12614
@@ -121,19 +83,41 @@ private fun getReoffendingProbability(totalForAllParameters: Double, x: Double):
   return numerator / denominator
 }
 
-private fun getAgeGroup(age: Int): AgeGroup = when {
-  age < 0 -> throw IllegalArgumentException("Age cannot be negative")
-  age < 10 -> throw IllegalArgumentException("Age must be age 10 or more")
-  age in 10..11 -> AgeGroup.TEN_TO_UNDER_TWELVE
-  age in 12..13 -> AgeGroup.TWELVE_TO_UNDER_FOURTEEN
-  age in 14..15 -> AgeGroup.FOURTEEN_TO_UNDER_SIXTEEN
-  age in 16..17 -> AgeGroup.SIXTEEN_TO_UNDER_EIGHTEEN
-  age in 18..20 -> AgeGroup.EIGHTEEN_TO_UNDER_TWENTY_ONE
-  age in 21..24 -> AgeGroup.TWENTY_ONE_TO_UNDER_TWENTY_FIVE
-  age in 25..29 -> AgeGroup.TWENTY_FIVE_TO_UNDER_THIRTY
-  age in 30..34 -> AgeGroup.THIRTY_TO_UNDER_THIRTY_FIVE
-  age in 35..39 -> AgeGroup.THIRTY_FIVE_TO_UNDER_FORTY
-  age in 40..49 -> AgeGroup.FORTY_TO_UNDER_FIFTY
-  age >= 50 -> AgeGroup.FIFTY_AND_OVER
-  else -> throw IllegalArgumentException("Unhandled age: $age")
+fun getAgeGenderParameter(age: Int, gender: Gender): Double {
+  val maleParams = listOf(
+    10..11 to 0.0,
+    12..13 to 0.08392,
+    14..15 to 0.07578,
+    16..17 to -0.0616,
+    18..20 to -0.6251,
+    21..24 to -1.0515,
+    25..29 to -1.1667,
+    30..34 to -1.326,
+    35..39 to -1.368,
+    40..49 to -1.4997,
+    50..Int.MAX_VALUE to -2.0253,
+  )
+
+  val femaleParams = listOf(
+    10..11 to 0.785,
+    12..13 to 0.61385,
+    14..15 to 0.66952,
+    16..17 to -0.9592,
+    18..20 to -0.8975,
+    21..24 to -1.0285,
+    25..29 to -1.0528,
+    30..34 to -1.1291,
+    35..39 to -1.4219,
+    40..49 to -1.5247,
+    50..Int.MAX_VALUE to -2.4498,
+  )
+
+  val params = when (gender) {
+    Gender.MALE -> maleParams
+    Gender.FEMALE -> femaleParams
+  }
+
+  return params.firstOrNull { age in it.first }
+    ?.second
+    ?: throw IllegalArgumentException("Unhandled age: $age")
 }
--- src/test/kotlin/uk/gov/justice/digital/hmpps/arnsriskactuarialapi/service/transformation/Ogrs3TransformationHelperTest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/arnsriskactuarialapi/service/transformation/Ogrs3TransformationHelperTest.kt
@@ -1,10 +1,11 @@
 package uk.gov.justice.digital.hmpps.arnsriskactuarialapi.service.transformation
 
-import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Assertions.assertThrows
+import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.TestInstance
-import uk.gov.justice.digital.hmpps.arnsriskactuarialapi.dto.AgeGroup
 import uk.gov.justice.digital.hmpps.arnsriskactuarialapi.dto.Gender
 import java.math.BigDecimal
 import java.time.LocalDate
@@ -12,133 +13,119 @@ import java.time.LocalDate
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class Ogrs3TransformationHelperTest {
 
-    private val today = LocalDate.of(2025, 1, 1)
+  private val today = LocalDate.of(2025, 1, 1)
 
-    @TestInstance(TestInstance.Lifecycle.PER_CLASS)
-    @Nested
-    inner class AgeTest {
+  @TestInstance(TestInstance.Lifecycle.PER_CLASS)
+  @Nested
+  inner class AgeTest {
 
-        @Test
-        fun `getAgeAtCurrentConviction age should be greater than min value`() {
-            val dob = today.minusYears(10)
-            val result = getAgeAtCurrentConviction(dob, today, ageAtFirstSanction = 10)
-            assertTrue(result.isSuccess)
-            assertEquals(10, result.getOrNull())
-        }
-
-        @Test
-        fun `getAgeAtCurrentConviction should round down months`() {
-            val dob = today.minusYears(10).minusMonths(3)
-            val result = getAgeAtCurrentConviction(dob, today, ageAtFirstSanction = 10)
-            assertTrue(result.isSuccess)
-            assertEquals(10, result.getOrNull())
-        }
-
-        @Test
-        fun `getAgeAtCurrentConviction age less than min should return error`() {
-            val dob = today.minusYears(9)
-            val result = getAgeAtCurrentConviction(dob, today, ageAtFirstSanction = 18)
-            assertTrue(result.isFailure)
-            assertEquals("Age at current conviction must be at least 10.", result.exceptionOrNull()?.message)
-        }
-
-        @Test
-        fun `getAgeAtCurrentConviction null dateOfBirth should return error`() {
-            val result = runCatching {
-                getAgeAtCurrentConviction(
-                    dateOfBirth = LocalDate.parse(null),
-                    dateOfCurrentConviction = today,
-                    ageAtFirstSanction = 18,
-                )
-            }
-            assertTrue(result.isFailure)
-        }
-
-        @Test
-        fun `getAgeAtCurrentConviction greater than current age should return error`() {
-            val dob = today.minusYears(15)
-            val result = getAgeAtCurrentConviction(dob, today, ageAtFirstSanction = 20)
-            assertTrue(result.isFailure)
-            assertEquals(
-                "Age at first sanction cannot be greater than age at current conviction.",
-                result.exceptionOrNull()?.message,
-            )
-        }
-
-        @Test
-        fun `getAgeAtStartOfFollowup exact`(){
-            val dob = today.minusYears(15)
-            assertEquals(
-                15,
-                getAgeAtStartOfFollowup(dob, today),
-            )
-        }
-
-        @Test
-        fun `getAgeAtStartOfFollowup round down`(){
-            val dob = today.minusYears(15).minusMonths(3)
-            assertEquals(
-                15,
-                getAgeAtStartOfFollowup(dob, today),
-            )
-        }
+    @Test
+    fun `getAgeAtCurrentConviction age should be greater than min value`() {
+      val dob = today.minusYears(10)
+      val result = getAgeAtCurrentConviction(dob, today, ageAtFirstSanction = 10)
+      assertTrue(result.isSuccess)
+      assertEquals(10, result.getOrNull())
     }
 
-    @TestInstance(TestInstance.Lifecycle.PER_CLASS)
-    @Nested
-    inner class OffenderCopasScoreTest {
-        @Test
-        fun `getOffenderCopasScore should produce the copas score`() {
-            val score = getOffenderCopasScore(3, 30, 20)
-            val expected = BigDecimal("-1.60944") // TODO: find some test data to compare against
-            assertEquals(expected, score)
-        }
-
-        @Test
-        fun `getOffenderCopasScore should throw error when denominator less or equal zero`() {
-            val exception = assertThrows(IllegalArgumentException::class.java) {
-                getOffenderCopasScore(0, 1, 18)
-            }
-            assert(exception.message!!.contains("Invalid age values leading to non-positive denominator"))
-        }
+    @Test
+    fun `getAgeAtCurrentConviction should round down months`() {
+      val dob = today.minusYears(10).minusMonths(3)
+      val result = getAgeAtCurrentConviction(dob, today, ageAtFirstSanction = 10)
+      assertTrue(result.isSuccess)
+      assertEquals(10, result.getOrNull())
     }
 
-
-    @TestInstance(TestInstance.Lifecycle.PER_CLASS)
-    @Nested
-    inner class AgeGenderParameterTest {
-        @Test
-        fun `getOffenderAgeGroup should return age group`() {
-            val actual = getOffenderAgeGroup(10)
-            assertEquals(AgeGroup.TEN_TO_UNDER_TWELVE, actual)
-        }
-
-        @Test
-        fun `getOffenderAgeGroup should throw error when under ten`() {
-            val exception = assertThrows(IllegalArgumentException::class.java) {
-                getOffenderAgeGroup(9)
-            }
-            assert(exception.message!!.contains("Age must be age 10 or more"))
-        }
-
-        @Test
-        fun `getOffenderAgeGroup should throw error when negative value`() {
-            val exception = assertThrows(IllegalArgumentException::class.java) {
-                getOffenderAgeGroup(-1)
-            }
-            assert(exception.message!!.contains("Age cannot be negative"))
-        }
-
-        @Test
-        fun `getAgeGenderParameter should return the current weight for the age group when male`() {
-            val actual = getAgeGenderParameter(AgeGroup.TEN_TO_UNDER_TWELVE, Gender.MALE)
-            assertEquals(0.0, actual)
-        }
-
-        @Test
-        fun `getAgeGenderParameter should return the current weight for the age group when female`() {
-            val actual = getAgeGenderParameter(AgeGroup.TEN_TO_UNDER_TWELVE, Gender.FEMALE)
-            assertEquals(0.785, actual)
-        }
+    @Test
+    fun `getAgeAtCurrentConviction age less than min should return error`() {
+      val dob = today.minusYears(9)
+      val result = getAgeAtCurrentConviction(dob, today, ageAtFirstSanction = 18)
+      assertTrue(result.isFailure)
+      assertEquals("Age at current conviction must be at least 10.", result.exceptionOrNull()?.message)
     }
+
+    @Test
+    fun `getAgeAtCurrentConviction null dateOfBirth should return error`() {
+      val result = runCatching {
+        getAgeAtCurrentConviction(
+          dateOfBirth = LocalDate.parse(null),
+          dateOfCurrentConviction = today,
+          ageAtFirstSanction = 18,
+        )
+      }
+      assertTrue(result.isFailure)
+    }
+
+    @Test
+    fun `getAgeAtCurrentConviction greater than current age should return error`() {
+      val dob = today.minusYears(15)
+      val result = getAgeAtCurrentConviction(dob, today, ageAtFirstSanction = 20)
+      assertTrue(result.isFailure)
+      assertEquals(
+        "Age at first sanction cannot be greater than age at current conviction.",
+        result.exceptionOrNull()?.message,
+      )
+    }
+
+    @Test
+    fun `getAgeAtStartOfFollowup exact`() {
+      val dob = today.minusYears(15)
+      assertEquals(
+        15,
+        getAgeAtStartOfFollowup(dob, today),
+      )
+    }
+
+    @Test
+    fun `getAgeAtStartOfFollowup round down`() {
+      val dob = today.minusYears(15).minusMonths(3)
+      assertEquals(
+        15,
+        getAgeAtStartOfFollowup(dob, today),
+      )
+    }
+  }
+
+  @TestInstance(TestInstance.Lifecycle.PER_CLASS)
+  @Nested
+  inner class OffenderCopasScoreTest {
+    @Test
+    fun `getOffenderCopasScore should produce the copas score`() {
+      val score = getOffenderCopasScore(3, 30, 20)
+      val expected = BigDecimal("-1.60944") // TODO: find some test data to compare against
+      assertEquals(expected, score)
+    }
+
+    @Test
+    fun `getOffenderCopasScore should throw error when denominator less or equal zero`() {
+      val exception = assertThrows(IllegalArgumentException::class.java) {
+        getOffenderCopasScore(0, 1, 18)
+      }
+      assert(exception.message!!.contains("Invalid age values leading to non-positive denominator"))
+    }
+  }
+
+  @TestInstance(TestInstance.Lifecycle.PER_CLASS)
+  @Nested
+  inner class AgeGenderParameterTest {
+
+    @Test
+    fun `getOffenderAgeGroup should throw error when under ten`() {
+      val exception = assertThrows(IllegalArgumentException::class.java) {
+        getAgeGenderParameter(9, Gender.MALE)
+      }
+      assert(exception.message!!.contains("Unhandled age: 9"))
+    }
+
+    @Test
+    fun `getAgeGenderParameter should return the current weight for the age group when male`() {
+      val actual = getAgeGenderParameter(11, Gender.MALE)
+      assertEquals(0.0, actual)
+    }
+
+    @Test
+    fun `getAgeGenderParameter should return the current weight for the age group when female`() {
+      val actual = getAgeGenderParameter(11, Gender.FEMALE)
+      assertEquals(0.785, actual)
+    }
+  }
 }
