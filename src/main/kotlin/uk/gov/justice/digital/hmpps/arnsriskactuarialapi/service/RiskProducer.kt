a5f86bb3f4ac9041a7f3d411c5a2765b2d811bcf
riccardo-noviello-moj-2 <Riccardo.Noviello@justice.gov.uk>
2025-07-16 09:39:01 +0100

    ACT-3: add missing tests

--- src/main/kotlin/uk/gov/justice/digital/hmpps/arnsriskactuarialapi/service/RiskProducer.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/arnsriskactuarialapi/service/RiskProducer.kt
@@ -3,6 +3,5 @@ package uk.gov.justice.digital.hmpps.arnsriskactuarialapi.service
 import uk.gov.justice.digital.hmpps.arnsriskactuarialapi.dto.RiskScoreRequest
 
 interface RiskProducer<T> {
-  @Override
   fun getRiskScore(riskScoreRequest: RiskScoreRequest): T
 }
