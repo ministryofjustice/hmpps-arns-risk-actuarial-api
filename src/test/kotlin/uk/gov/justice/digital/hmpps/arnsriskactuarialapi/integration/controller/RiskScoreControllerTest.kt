48d861bac117e7936ef245a16031389657e58e02
riccardo-noviello-moj-2 <Riccardo.Noviello@justice.gov.uk>
2025-07-16 08:51:12 +0100

    ACT-3: fix failing tests

--- src/test/kotlin/uk/gov/justice/digital/hmpps/arnsriskactuarialapi/integration/controller/RiskScoreControllerTest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/arnsriskactuarialapi/integration/controller/RiskScoreControllerTest.kt
@@ -3,26 +3,31 @@ package uk.gov.justice.digital.hmpps.arnsriskactuarialapi.integration.controller
 import org.junit.jupiter.api.Test
 import org.mockito.Mockito.mock
 import org.mockito.kotlin.whenever
+import org.springframework.boot.test.mock.mockito.MockBean
+import org.springframework.test.context.bean.override.mockito.MockitoBean
+import uk.gov.justice.digital.hmpps.arnsriskactuarialapi.dto.Gender
 import uk.gov.justice.digital.hmpps.arnsriskactuarialapi.dto.OGRS3Object
 import uk.gov.justice.digital.hmpps.arnsriskactuarialapi.dto.RiskBand
 import uk.gov.justice.digital.hmpps.arnsriskactuarialapi.dto.RiskScoreRequest
 import uk.gov.justice.digital.hmpps.arnsriskactuarialapi.dto.RiskScoreResponse
 import uk.gov.justice.digital.hmpps.arnsriskactuarialapi.integration.IntegrationTestBase
 import uk.gov.justice.digital.hmpps.arnsriskactuarialapi.service.RiskScoreService
+import java.time.LocalDate
 
 class RiskScoreControllerTest : IntegrationTestBase() {
 
-  private val riskScoreService: RiskScoreService = mock()
+  @MockitoBean
+  private lateinit var riskScoreService: RiskScoreService
 
   private val basicRequest = RiskScoreRequest(
     "1_0",
-    null,
-    null,
-    null,
-    null,
-    null,
-    null,
-    null,
+    Gender.MALE,
+    LocalDate.of(1964, 10, 15),
+    LocalDate.of(2014, 12, 13),
+    LocalDate.of(2027, 12, 12),
+    10,
+    30,
+    "051101",
   )
 
   @Test
@@ -46,7 +51,11 @@ class RiskScoreControllerTest : IntegrationTestBase() {
       .expectStatus()
       .isOk
       .expectBody()
-      .jsonPath("$.ogrs3.algorithmVersion").isEqualTo("1_0")
+      .jsonPath("$.ogrs3.algorithmVersion").isEqualTo(ogrs3.algorithmVersion)
+      .jsonPath("$.ogrs3.ogrs3OneYear").isEqualTo(ogrs3.ogrs3OneYear)
+      .jsonPath("$.ogrs3.ogrs3TwoYear").isEqualTo(ogrs3.ogrs3TwoYear)
+      .jsonPath("$.ogrs3.band").isEqualTo(ogrs3.band)
+      .jsonPath("$.ogrs3.validationError").isEmpty()
   }
 
   @Test
--- src/test/kotlin/uk/gov/justice/digital/hmpps/arnsriskactuarialapi/service/RiskScoreServiceTest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/arnsriskactuarialapi/service/RiskScoreServiceTest.kt
@@ -2,10 +2,22 @@ package uk.gov.justice.digital.hmpps.arnsriskactuarialapi.service
 
 import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.ExtendWith
+import org.mockito.InjectMocks
+import org.mockito.Mock
+import org.mockito.junit.jupiter.MockitoExtension
+import org.mockito.kotlin.whenever
+import uk.gov.justice.digital.hmpps.arnsriskactuarialapi.dto.OGRS3Object
 import uk.gov.justice.digital.hmpps.arnsriskactuarialapi.dto.RiskScoreRequest
 
+@ExtendWith(MockitoExtension::class)
 class RiskScoreServiceTest {
-  private val riskScoreService = RiskScoreService()
+
+  @Mock
+  private lateinit var oGRS3RiskScoreService: OGRS3RiskScoreService
+
+  @InjectMocks
+  private lateinit var riskScoreService: RiskScoreService
 
   @Test
   fun `riskScoreProducer returns risk score response with algorithm version as 1_0`() {
@@ -19,6 +31,10 @@ class RiskScoreServiceTest {
       null,
       null,
     )
+
+    whenever(oGRS3RiskScoreService.getRiskScore(request))
+      .thenReturn(OGRS3Object("1_0", null, null, null, null))
+
 
     val result = riskScoreService.riskScoreProducer(request)
     Assertions.assertEquals("1_0", result.OGRS3.algorithmVersion)
