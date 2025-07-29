b491922880b9be81844398d54bf54d33f1ac40d7
riccardo-noviello-moj-2 <Riccardo.Noviello@justice.gov.uk>
2025-07-29 12:07:36 +0100

    ACT-71: add eligibility check

--- src/main/kotlin/uk/gov/justice/digital/hmpps/arnsriskactuarialapi/service/OPDRiskProducerService.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/arnsriskactuarialapi/service/OPDRiskProducerService.kt
@@ -1,13 +1,21 @@
 package uk.gov.justice.digital.hmpps.arnsriskactuarialapi.service
 
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Service
+import uk.gov.justice.digital.hmpps.arnsriskactuarialapi.dto.Gender
+import uk.gov.justice.digital.hmpps.arnsriskactuarialapi.dto.RiskBand
 import uk.gov.justice.digital.hmpps.arnsriskactuarialapi.dto.RiskScoreContext
 import uk.gov.justice.digital.hmpps.arnsriskactuarialapi.dto.RiskScoreRequest
 import uk.gov.justice.digital.hmpps.arnsriskactuarialapi.dto.opd.OPDObject
+import uk.gov.justice.digital.hmpps.arnsriskactuarialapi.dto.opd.OPDRequestValidated
 import uk.gov.justice.digital.hmpps.arnsriskactuarialapi.service.validation.opdInitialValidation
 
 @Service
 class OPDRiskProducerService : RiskScoreProducer {
+
+  @Autowired
+  lateinit var offenceGroupParametersService: OffenceGroupParametersService
+
   override fun getRiskScore(
     request: RiskScoreRequest,
     context: RiskScoreContext,
@@ -19,7 +27,93 @@ class OPDRiskProducerService : RiskScoreProducer {
         .copy(OPD = OPDObject(opdEligibility = false, opdCheck = null, errors))
     }
 
-    // TODO: transformation
-    return context.copy(OPD = OPDObject(opdEligibility = false, opdCheck = null, validationError = emptyList()))
+    // request validated
+    val validatedRequest = OPDRequestValidated(
+      overallRiskForAssessment = request.overallRiskForAssessment!!,
+      highestRiskLevel = request.highestRiskLevel!!,
+      gender = request.gender!!,
+      currentOffence = request.currentOffence!!,
+      custodialSentence = request.custodialSentence!!,
+      currentPsychologicalProblems = request.currentPsychologicalProblems,
+      experienceOfChildhood = request.experienceOfChildhood,
+      difficultiesCoping = request.difficultiesCoping,
+      domesticAbusePartner = request.domesticAbusePartner,
+      domesticAbuseFamily = request.domesticAbuseFamily,
+      ageAtFirstSanction = request.ageAtFirstSanction,
+      financialRelianceOnOthers = request.financialRelianceOnOthers,
+      manipulativePredatoryBehaviour = request.manipulativePredatoryBehaviour,
+      childhoodBehaviour = request.childhoodBehaviour,
+      currentPsychiatricProblems = request.currentPsychiatricProblems,
+      attitudesStableBehaviour = request.attitudesStableBehaviour,
+      impulsivityBehaviour = request.impulsivityBehaviour,
+      attitudeTowardsSupervision = request.attitudeTowardsSupervision,
+      opdOverride = request.opdOverride ?: false,
+      eligibleForMappa = request.eligibleForMappa ?: false,
+      carryingOrUsingWeapon = request.carryingOrUsingWeapon ?: false,
+      violenceOrThreatOfViolence = request.violenceOrThreatOfViolence ?: false,
+      excessiveOrSadisticViolence = request.excessiveOrSadisticViolence ?: false,
+      offenceArson = request.offenceArson ?: false,
+      offenderMotivations = request.offenderMotivations ?: false,
+      offenceLinkedRiskOfSeriousHarm = request.offenceLinkedRiskOfSeriousHarm ?: false,
+      accommodationLinkedRiskOfSeriousHarm = request.accommodationLinkedRiskOfSeriousHarm ?: false,
+      domesticAbuse = request.domesticAbuse ?: false,
+      relationshipLinkedSeriousHarm = request.relationshipLinkedSeriousHarm ?: false,
+      wellbeingEmotionalLinkedRiskOfSeriousHarm = request.wellbeingEmotionalLinkedRiskOfSeriousHarm ?: false,
+      thinkingAndBehaviourLinedToRiskOfSeriousHarm = request.thinkingAndBehaviourLinedToRiskOfSeriousHarm
+        ?: false,
+      historyOfPsychiatricTreatment = request.historyOfPsychiatricTreatment ?: false,
+      medicationMentalHealth = request.medicationMentalHealth ?: false,
+      patientSecureUnitOrHospital = request.patientSecureUnitOrHospital ?: false,
+      obsessiveBehaviour = request.obsessiveBehaviour ?: false,
+      selfHarmSuicideAttempt = request.selfHarmSuicideAttempt ?: false,
+      concernsAboutSuicidePast = request.concernsAboutSuicidePast ?: false,
+      concernsAboutSelfHarmPast = request.concernsAboutSelfHarmPast ?: false,
+      assaultedOrThreatenedStaff = request.assaultedOrThreatenedStaff ?: false,
+      escapeOrAbsconded = request.escapeOrAbsconded ?: false,
+      controlIssues = request.controlIssues ?: false,
+      breachOfTrust = request.breachOfTrust ?: false,
+      impactOfOffendingOnOthers = request.impactOfOffendingOnOthers ?: false,
+    )
+
+    // transformation
+    val isViolentOrSexualType =
+      offenceGroupParametersService.isViolentOrSexualType(validatedRequest.currentOffence)
+
+    val isOpdApplicable = when (validatedRequest.gender) {
+      Gender.MALE -> isOpdApplicableMale(validatedRequest, isViolentOrSexualType)
+      Gender.FEMALE -> isOpdApplicableFemale(validatedRequest)
+    }
+
+    if (!isOpdApplicable) {
+      return context.copy(
+        OPD = OPDObject(
+          opdEligibility = false,
+          opdCheck = null,
+          validationError = emptyList(),
+        ),
+      )
+    }
+
+    // proceed with score
+
+    return context.copy(
+      OPD = OPDObject(
+        opdEligibility = true,
+        opdCheck = null,
+        validationError = emptyList(),
+      ),
+    )
   }
+
+  fun isOpdApplicableMale(
+    request: OPDRequestValidated,
+    isViolentOrSexualType: Boolean,
+  ): Boolean = (request.overallRiskForAssessment in arrayOf(RiskBand.HIGH, RiskBand.VERY_HIGH)) &&
+    isViolentOrSexualType &&
+    request.custodialSentence
+
+  fun isOpdApplicableFemale(
+    request: OPDRequestValidated,
+  ): Boolean = (request.overallRiskForAssessment in arrayOf(RiskBand.HIGH, RiskBand.VERY_HIGH)) ||
+    request.eligibleForMappa
 }
--- src/test/kotlin/uk/gov/justice/digital/hmpps/arnsriskactuarialapi/service/OPDRiskProducerServiceTest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/arnsriskactuarialapi/service/OPDRiskProducerServiceTest.kt
@@ -4,26 +4,103 @@ import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertNotNull
+import org.junit.jupiter.api.extension.ExtendWith
+import org.mockito.InjectMocks
+import org.mockito.Mock
+import org.mockito.junit.jupiter.MockitoExtension
+import org.mockito.kotlin.whenever
+import uk.gov.justice.digital.hmpps.arnsriskactuarialapi.dto.Gender
+import uk.gov.justice.digital.hmpps.arnsriskactuarialapi.dto.RiskBand
 import uk.gov.justice.digital.hmpps.arnsriskactuarialapi.dto.ValidationErrorType
 import uk.gov.justice.digital.hmpps.arnsriskactuarialapi.emptyContext
 import uk.gov.justice.digital.hmpps.arnsriskactuarialapi.emptyOPD
 import uk.gov.justice.digital.hmpps.arnsriskactuarialapi.validOPDRiskScoreRequest
 
+@ExtendWith(MockitoExtension::class)
 class OPDRiskProducerServiceTest {
 
-  private val service = OPDRiskProducerService()
+  @Mock
+  lateinit var offenceGroupParametersService: OffenceGroupParametersService
+
+  @InjectMocks
+  lateinit var service: OPDRiskProducerService
 
   @Test
-  fun `should calculate empty OPD with an valid request`() { // TODO will change in next transformation ticket
+  fun `should calculate OPD with an valid request, eligible  male`() {
+    whenever(offenceGroupParametersService.isViolentOrSexualType("02504")).thenReturn(true)
+
     val context = emptyContext().copy(
       OPD = emptyOPD(),
     )
-    val request = validOPDRiskScoreRequest()
+    val request = validOPDRiskScoreRequest().copy(
+      currentOffence = "02504",
+      gender = Gender.MALE,
+      overallRiskForAssessment = RiskBand.HIGH,
+      custodialSentence = true,
+    )
 
     val result = service.getRiskScore(request, context).OPD
     assertNotNull(result)
-    assertEquals(false, result.opdEligibility)
+    assertEquals(true, result.opdEligibility)
     assertTrue(result.validationError?.isEmpty() == true)
+    // TODO: check score
+  }
+
+  @Test
+  fun `should calculate OPD with an valid request, eligible  female`() {
+    whenever(offenceGroupParametersService.isViolentOrSexualType("02504")).thenReturn(true)
+
+    val context = emptyContext().copy(
+      OPD = emptyOPD(),
+    )
+    val request = validOPDRiskScoreRequest().copy(
+      currentOffence = "02504",
+      gender = Gender.FEMALE,
+      overallRiskForAssessment = RiskBand.HIGH,
+      eligibleForMappa = true,
+    )
+
+    val result = service.getRiskScore(request, context).OPD
+    assertNotNull(result)
+    assertEquals(true, result.opdEligibility)
+    assertTrue(result.validationError?.isEmpty() == true)
+    // TODO: check score
+  }
+
+  @Test
+  fun `should not calculate OPD with an valid request, not-eligible  female`() {
+    whenever(offenceGroupParametersService.isViolentOrSexualType("02504")).thenReturn(true)
+
+    val context = emptyContext().copy(
+      OPD = emptyOPD(),
+    )
+    val request = validOPDRiskScoreRequest().copy(
+      currentOffence = "02504",
+      gender = Gender.FEMALE,
+      overallRiskForAssessment = RiskBand.LOW,
+      eligibleForMappa = false,
+    )
+
+    val result = service.getRiskScore(request, context).OPD
+    assertEquals(false, result?.opdEligibility)
+  }
+
+  @Test
+  fun `should not calculate OPD with an valid request, not-eligible  male`() {
+    whenever(offenceGroupParametersService.isViolentOrSexualType("02504")).thenReturn(true)
+
+    val context = emptyContext().copy(
+      OPD = emptyOPD(),
+    )
+    val request = validOPDRiskScoreRequest().copy(
+      currentOffence = "02504",
+      gender = Gender.MALE,
+      overallRiskForAssessment = RiskBand.LOW,
+      custodialSentence = false,
+    )
+
+    val result = service.getRiskScore(request, context).OPD
+    assertEquals(false, result?.opdEligibility)
   }
 
   @Test
