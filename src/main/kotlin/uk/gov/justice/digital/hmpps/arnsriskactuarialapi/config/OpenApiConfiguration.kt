7f2cfaf4e1c6d6bce5b32efeb0476d306364292c
James Boobier <139986710+JamesBoobier@users.noreply.github.com>
2025-08-12 16:10:40 +0100

    ACT-60 Update swagger documentation and config

--- src/main/kotlin/uk/gov/justice/digital/hmpps/arnsriskactuarialapi/config/OpenApiConfiguration.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/arnsriskactuarialapi/config/OpenApiConfiguration.kt
@@ -1,8 +1,10 @@
 package uk.gov.justice.digital.hmpps.arnsriskactuarialapi.config
 
+import io.swagger.v3.oas.models.Components
 import io.swagger.v3.oas.models.OpenAPI
 import io.swagger.v3.oas.models.info.Contact
 import io.swagger.v3.oas.models.info.Info
+import io.swagger.v3.oas.models.security.SecurityRequirement
 import io.swagger.v3.oas.models.security.SecurityScheme
 import io.swagger.v3.oas.models.servers.Server
 import org.springframework.boot.info.BuildProperties
@@ -27,10 +29,23 @@ class OpenApiConfiguration(buildProperties: BuildProperties) {
       listOf(),
     )
     .info(
-      Info().title("HMPPS Arns Risk Actuarial Api").version(version)
+      Info().title("HMPPS ARNS Risk Actuarial Api").version(version)
+        .description(
+          """
+          |API for obtaining risk scores based on data captured in an ARNS assessment.
+          |
+          |For full details on using this API see this [Confluence](https://dsdmoj.atlassian.net/wiki/spaces/APP/pages/5781389446/Risk+actuarial+API+spec) page.
+          """.trimMargin(),
+        )
         .contact(Contact().name("HMPPS Digital Studio").email("feedback@digital.justice.gov.uk")),
     )
-  // TODO Add security schema and roles in `.components()` and `.addSecurityItem()`
+    .components(
+      Components().addSecuritySchemes(
+        "arns-risk-actuarial-api-role",
+        SecurityScheme().addBearerJwtRequirement(ROLE_ARNS_RISK_ACTUARIAL),
+      ),
+    )
+    .addSecurityItem(SecurityRequirement().addList("arns-risk-actuarial-api-role", listOf("read")))
 }
 
 private fun SecurityScheme.addBearerJwtRequirement(role: String): SecurityScheme = type(SecurityScheme.Type.HTTP)
--- src/main/kotlin/uk/gov/justice/digital/hmpps/arnsriskactuarialapi/controller/RiskScoreController.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/arnsriskactuarialapi/controller/RiskScoreController.kt
@@ -1,19 +1,48 @@
 package uk.gov.justice.digital.hmpps.arnsriskactuarialapi.controller
 
+import io.swagger.v3.oas.annotations.Operation
+import io.swagger.v3.oas.annotations.media.Content
+import io.swagger.v3.oas.annotations.media.Schema
+import io.swagger.v3.oas.annotations.responses.ApiResponse
+import io.swagger.v3.oas.annotations.security.SecurityRequirement
 import org.springframework.security.access.prepost.PreAuthorize
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
+import uk.gov.justice.digital.hmpps.arnsriskactuarialapi.config.ROLE_ARNS_RISK_ACTUARIAL
 import uk.gov.justice.digital.hmpps.arnsriskactuarialapi.dto.RiskScoreRequest
 import uk.gov.justice.digital.hmpps.arnsriskactuarialapi.dto.RiskScoreResponse
 import uk.gov.justice.digital.hmpps.arnsriskactuarialapi.service.RiskScoreService
+import uk.gov.justice.hmpps.kotlin.common.ErrorResponse
 
 @RestController
 @RequestMapping("/risk-scores/v1")
 class RiskScoreController(private val riskScoreService: RiskScoreService) {
 
   @PostMapping
-  @PreAuthorize("hasRole('ARNS_RISK_ACTUARIAL')")
+  @PreAuthorize("hasRole('$ROLE_ARNS_RISK_ACTUARIAL')")
+  @Operation(
+    summary = "Calculate risk scores from given inputs",
+    description = "Takes in parameters and returns a set of risk scores for use in ARNS",
+    responses = [
+      ApiResponse(
+        responseCode = "200",
+        description = "Successfully calculated risks scores, returned in response body.",
+        content = [Content(mediaType = "application/json", schema = Schema(implementation = RiskScoreRequest::class))],
+      ),
+      ApiResponse(
+        responseCode = "400",
+        description = "Request body does not match schema",
+        content = [Content(mediaType = "application/json", schema = Schema(implementation = ErrorResponse::class))],
+      ),
+      ApiResponse(
+        responseCode = "403",
+        description = "Insufficient permissions to call endpoint",
+        content = [Content(mediaType = "application/json", schema = Schema(implementation = ErrorResponse::class))],
+      ),
+    ],
+    security = [SecurityRequirement(name = "arns-risk-actuarial-api-role")],
+  )
   fun postRiskScores(@RequestBody request: RiskScoreRequest): RiskScoreResponse = riskScoreService.riskScoreProducer(request)
 }
