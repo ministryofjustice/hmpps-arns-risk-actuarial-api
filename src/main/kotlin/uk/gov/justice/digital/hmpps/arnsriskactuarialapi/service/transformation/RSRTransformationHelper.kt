bd7693df467bf952d9f5a1799ee23d3814c2fbfe
riccardo-noviello-moj-2 <Riccardo.Noviello@justice.gov.uk>
2025-08-18 16:40:39 +0100

    FIx rsr band calculation (#83)

--- src/main/kotlin/uk/gov/justice/digital/hmpps/arnsriskactuarialapi/service/transformation/RSRTransformationHelper.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/arnsriskactuarialapi/service/transformation/RSRTransformationHelper.kt
@@ -13,7 +13,7 @@ fun getRSRBand(rsrScore: Double?): RiskBand {
   if (rsrScore == null) throw IllegalArgumentException("RSR Score is null")
   return when {
     rsrScore >= 0.0 && rsrScore <= 3.0 -> RiskBand.LOW
-    rsrScore > 3.0 && rsrScore <= 6.8 -> RiskBand.MEDIUM
+    rsrScore > 3.0 && rsrScore < 6.9 -> RiskBand.MEDIUM
     rsrScore >= 6.9 -> RiskBand.HIGH
     else -> throw IllegalArgumentException("RSR Score out of supported range: $rsrScore")
   }
