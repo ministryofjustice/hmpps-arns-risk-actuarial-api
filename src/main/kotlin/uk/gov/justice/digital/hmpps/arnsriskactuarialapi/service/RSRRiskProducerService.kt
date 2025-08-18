ceb9fbd9870b549638531a1086e456223b77f70e
riccardo-noviello-moj-2 <Riccardo.Noviello@justice.gov.uk>
2025-08-18 14:50:37 +0100

    ACT-126: Fix Rounding of RSR scores as percentages (#81)

--- src/main/kotlin/uk/gov/justice/digital/hmpps/arnsriskactuarialapi/service/RSRRiskProducerService.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/arnsriskactuarialapi/service/RSRRiskProducerService.kt
@@ -5,31 +5,49 @@ import uk.gov.justice.digital.hmpps.arnsriskactuarialapi.dto.RiskScoreContext
 import uk.gov.justice.digital.hmpps.arnsriskactuarialapi.dto.RiskScoreRequest
 import uk.gov.justice.digital.hmpps.arnsriskactuarialapi.dto.ValidationErrorResponse
 import uk.gov.justice.digital.hmpps.arnsriskactuarialapi.dto.ValidationErrorType
+import uk.gov.justice.digital.hmpps.arnsriskactuarialapi.dto.osp.OSPDCObject
+import uk.gov.justice.digital.hmpps.arnsriskactuarialapi.dto.ospiic.OSPIICObject
 import uk.gov.justice.digital.hmpps.arnsriskactuarialapi.dto.rsr.RSRObject
 import uk.gov.justice.digital.hmpps.arnsriskactuarialapi.dto.rsr.RSRRequestValidated
+import uk.gov.justice.digital.hmpps.arnsriskactuarialapi.dto.snsv.SNSVObject
 import uk.gov.justice.digital.hmpps.arnsriskactuarialapi.service.transformation.getFullRSRScore
 import uk.gov.justice.digital.hmpps.arnsriskactuarialapi.service.transformation.getRSRBand
+import uk.gov.justice.digital.hmpps.arnsriskactuarialapi.utils.asDoublePercentage
 
 @Service
 class RSRRiskProducerService : RiskScoreProducer {
 
   // todo - basic return to enable testing for ospdc and ospiic tickets
   override fun getRiskScore(request: RiskScoreRequest, context: RiskScoreContext): RiskScoreContext {
-    val ospdc = context.OSPDC!!
-    val ospiic = context.OSPIIC!!
-    val snsv = context.SNSV!! // todo these should never be null/empty, there will either be scores or validation exceptions
+    val ospdc = context.OSPDC ?: OSPDCObject(null, null, null)
+    val ospiic = context.OSPIIC ?: OSPIICObject(null, null, null)
+    val snsv =
+      context.SNSV ?: SNSVObject(null, null, null)
 
-    val errors = (ospdc.validationError ?: emptyList()) + (ospiic.validationError ?: emptyList()) + (snsv.validationError ?: emptyList()) // TODO cleanup
+    val errors = listOfNotNull(
+      ospdc.validationError,
+      ospiic.validationError,
+      snsv.validationError,
+    ).flatten()
 
-    // todo could be cleaner
     if (errors.isNotEmpty()) {
-      if (snsv.snsvScore != null) {
-        val snsvScoreType = snsv.scoreType
-        val rsrScore = getFullRSRScore(snsv.snsvScore, ospdc.ospdcScore, ospiic.score, snsvScoreType)
-        val rsrBand = getRSRBand(rsrScore)
-        return context.apply { RSR = RSRObject(ospdc.ospdcBand, ospdc.ospdcScore, ospiic.band, ospiic.score, rsrScore, rsrBand, snsvScoreType, null, errors) }
-      } else {
-        return context.apply { RSR = RSRObject(ospdc.ospdcBand, ospdc.ospdcScore, ospiic.band, ospiic.score, null, null, null, null, errors) }
+      val rsrScore = snsv.snsvScore?.let { snsvScore ->
+        getFullRSRScore(snsvScore, ospdc.ospdcScore, ospiic.score, snsv.scoreType)?.asDoublePercentage()
+      }
+      val rsrBand = rsrScore?.let { getRSRBand(it) }
+
+      return context.apply {
+        RSR = RSRObject(
+          ospdc.ospdcBand,
+          ospdc.ospdcScore?.asDoublePercentage(),
+          ospiic.band,
+          ospiic.score?.asDoublePercentage(),
+          rsrScore,
+          rsrBand,
+          snsv.snsvScore?.let { snsv.scoreType },
+          null,
+          errors,
+        )
       }
     }
 
@@ -47,9 +65,20 @@ class RSRRiskProducerService : RiskScoreProducer {
     request: RSRRequestValidated,
   ): RSRObject = runCatching {
     val snsvScoreType = request.snsv.scoreType
-    val rsrScore = getFullRSRScore(request.snsv.snsvScore, request.ospdc.ospdcScore, request.ospiic.score, snsvScoreType)
+    val rsrScore =
+      getFullRSRScore(request.snsv.snsvScore, request.ospdc.ospdcScore, request.ospiic.score, snsvScoreType)
     val rsrBand = getRSRBand(rsrScore)
-    RSRObject(request.ospdc.ospdcBand, request.ospdc.ospdcScore, request.ospiic.band, request.ospiic.score, rsrScore, rsrBand, snsvScoreType, null, null)
+    RSRObject(
+      request.ospdc.ospdcBand,
+      request.ospdc.ospdcScore,
+      request.ospiic.band,
+      request.ospiic.score,
+      rsrScore,
+      rsrBand,
+      snsvScoreType,
+      null,
+      null,
+    )
   }.getOrElse {
     RSRObject(
       request.ospdc.ospdcBand,
