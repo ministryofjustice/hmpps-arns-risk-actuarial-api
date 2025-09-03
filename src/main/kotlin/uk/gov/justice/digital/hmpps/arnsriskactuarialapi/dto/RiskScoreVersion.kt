e10455f2376050b57d7f319609a1c89c170d9ab8
riccardo-noviello-moj-2 <Riccardo.Noviello@justice.gov.uk>
2025-09-03 12:33:12 +0100

    ogrs4 versioned example

--- src/main/kotlin/uk/gov/justice/digital/hmpps/arnsriskactuarialapi/dto/RiskScoreVersion.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/arnsriskactuarialapi/dto/RiskScoreVersion.kt
@@ -2,6 +2,7 @@ package uk.gov.justice.digital.hmpps.arnsriskactuarialapi.dto
 
 enum class RiskScoreVersion {
   V1_0,
+  V2_0,
   ;
 
   companion object {
--- src/main/kotlin/uk/gov/justice/digital/hmpps/arnsriskactuarialapi/service/RiskScoreService.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/arnsriskactuarialapi/service/RiskScoreService.kt
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Service
 import uk.gov.justice.digital.hmpps.arnsriskactuarialapi.dto.RiskScoreContext
 import uk.gov.justice.digital.hmpps.arnsriskactuarialapi.dto.RiskScoreRequest
 import uk.gov.justice.digital.hmpps.arnsriskactuarialapi.dto.RiskScoreResponse
+import uk.gov.justice.digital.hmpps.arnsriskactuarialapi.dto.RiskScoreVersion
 import uk.gov.justice.digital.hmpps.arnsriskactuarialapi.dto.toRiskScoreResponse
 
 @Service
@@ -43,19 +44,41 @@ class RiskScoreService {
   @Autowired
   lateinit var ospiicRiskProducerService: OSPIICRiskProducerService
 
-  fun riskScoreProducer(riskScoreRequest: RiskScoreRequest): RiskScoreResponse = listOf(
-    ogrs3RiskProducerService,
-    ovpRiskProducerService,
-    ogpRiskProducerService,
-    mstRiskProducerService,
-    opdRiskProducerService,
-    pniRiskProducerService,
-    ldsRiskProducerService,
-    ospdcRiskProducerService,
-    snsvRiskProducerService,
-    ospiicRiskProducerService,
-    rsrRiskProducerService,
-  ).fold(RiskScoreContext(riskScoreRequest.version)) { context, service ->
-    service.getRiskScore(riskScoreRequest, context)
-  }.toRiskScoreResponse()
+  fun riskScoreProducer(riskScoreRequest: RiskScoreRequest): RiskScoreResponse {
+    val listOfVersion1Predictors = listOf(
+      ogrs3RiskProducerService,
+      ovpRiskProducerService,
+      ogpRiskProducerService,
+      mstRiskProducerService,
+      opdRiskProducerService,
+      pniRiskProducerService,
+      ldsRiskProducerService,
+      ospdcRiskProducerService,
+      snsvRiskProducerService,
+      ospiicRiskProducerService,
+      rsrRiskProducerService,
+    )
+    val listOfVersion2Predictors = listOf(
+      // TODO: ogrs4RiskProducerService,
+      ovpRiskProducerService,
+      ogpRiskProducerService,
+      mstRiskProducerService,
+      opdRiskProducerService,
+      pniRiskProducerService,
+      ldsRiskProducerService,
+      ospdcRiskProducerService,
+      snsvRiskProducerService,
+      ospiicRiskProducerService,
+      rsrRiskProducerService,
+    )
+    val versionedPredictorsList = when (riskScoreRequest.version) {
+      RiskScoreVersion.V1_0 -> listOfVersion1Predictors
+      RiskScoreVersion.V2_0 -> listOfVersion2Predictors
+      else -> emptyList()
+    }
+    return versionedPredictorsList.fold(RiskScoreContext(riskScoreRequest.version)) { context, service ->
+      service.getRiskScore(riskScoreRequest, context)
+    }.toRiskScoreResponse()
+  }
+
 }
