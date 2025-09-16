a7052b38d83bf9ceb7b8184ba7e27c0aa05a6c0d
riccardo-noviello-moj-2 <Riccardo.Noviello@justice.gov.uk>
2025-09-16 15:04:41 +0100

    ACT-176: Refactor LDS test to be API tests instead of CSV (#120)

--- src/test/kotlin/uk/gov/justice/digital/hmpps/arnsriskactuarialapi/service/LDSRiskProducerServiceTest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/arnsriskactuarialapi/service/LDSRiskProducerServiceTest.kt
@@ -3,37 +3,17 @@ package uk.gov.justice.digital.hmpps.arnsriskactuarialapi.service
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.TestInstance
-import org.junit.jupiter.params.ParameterizedTest
-import org.junit.jupiter.params.provider.CsvFileSource
 import uk.gov.justice.digital.hmpps.arnsriskactuarialapi.dto.ProblemLevel
 import uk.gov.justice.digital.hmpps.arnsriskactuarialapi.dto.RiskScoreContext
 import uk.gov.justice.digital.hmpps.arnsriskactuarialapi.dto.RiskScoreRequest
 import uk.gov.justice.digital.hmpps.arnsriskactuarialapi.dto.RiskScoreVersion
-import uk.gov.justice.digital.hmpps.arnsriskactuarialapi.dto.ValidationErrorResponse
-import uk.gov.justice.digital.hmpps.arnsriskactuarialapi.dto.ValidationErrorType
 import uk.gov.justice.digital.hmpps.arnsriskactuarialapi.dto.lds.HasQualifications
 import uk.gov.justice.digital.hmpps.arnsriskactuarialapi.dto.lds.LDSObject
-import uk.gov.justice.digital.hmpps.arnsriskactuarialapi.service.validation.LDSValidationHelper.Companion.ELIGIBLE_FIELDS
-import uk.gov.justice.digital.hmpps.arnsriskactuarialapi.service.validation.LDSValidationHelper.Companion.ERR_LESS_THAN_THREE_FIELDS
 
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class LDSRiskProducerServiceTest {
 
   companion object {
-
-    const val NOT_ENOUGH_ITEMS_SCORED = "Not enough items scored"
-
-    val LDS_ERROR_OBJECT =
-      LDSObject(
-        null,
-        listOf(
-          ValidationErrorResponse(
-            type = ValidationErrorType.NOT_APPLICABLE,
-            message = ERR_LESS_THAN_THREE_FIELDS,
-            fields = ELIGIBLE_FIELDS,
-          ),
-        ),
-      )
 
     fun String?.toLDSBoolean() = when (this) {
       null -> null
@@ -89,45 +69,6 @@ class LDSRiskProducerServiceTest {
     val expected = RiskScoreContext(
       RiskScoreVersion.V1_0,
       LDS = LDSObject(0, emptyList()),
-    )
-    assertEquals(expected, result)
-  }
-
-  @ParameterizedTest(name = "[{index}] {arguments}")
-  @CsvFileSource(
-    resources = ["/data/LDS/LDS_Test_Data.csv"],
-    useHeadersInDisplayName = true,
-    nullValues = ["Null", "Missing"],
-  )
-  fun `requests from CSV file`(
-    isCurrentlyOfNoFixedAbodeOrTransientAccommodationCSV: String?,
-    workRelatedSkillsCSV: String?,
-    problemsWithReadingWritingNumeracyCSV: String?,
-    hasProblemsWithReadingCSV: String?,
-    hasProblemsWithNumeracyCSV: String?,
-    learningDifficultiesCSV: String?,
-    professionalOrVocationalQualificationsCSV: String?,
-    @Suppress("UNUSED_PARAMETER") ldsSubTotalCSV: String?,
-    ldsScoreCSV: String?,
-    commentsCSV: String?,
-  ) {
-    val request = toRequest(
-      isCurrentlyOfNoFixedAbodeOrTransientAccommodationCSV = isCurrentlyOfNoFixedAbodeOrTransientAccommodationCSV,
-      workRelatedSkillsCSV = workRelatedSkillsCSV,
-      problemsWithReadingWritingNumeracyCSV = problemsWithReadingWritingNumeracyCSV,
-      hasProblemsWithReadingCSV = hasProblemsWithReadingCSV,
-      hasProblemsWithNumeracyCSV = hasProblemsWithNumeracyCSV,
-      learningDifficultiesCSV = learningDifficultiesCSV,
-      professionalOrVocationalQualificationsCSV = professionalOrVocationalQualificationsCSV,
-    )
-    val result = service.getRiskScore(request, context)
-    val expected = RiskScoreContext(
-      RiskScoreVersion.V1_0,
-      LDS = if (NOT_ENOUGH_ITEMS_SCORED == commentsCSV) {
-        LDS_ERROR_OBJECT
-      } else {
-        LDSObject(ldsScoreCSV!!.toInt(), emptyList())
-      },
     )
     assertEquals(expected, result)
   }
