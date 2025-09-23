c71a304333145e5197bee3ee49856dca81ab6863
Jordan Hill <jordan.hill@justice.gov.uk>
2025-09-23 10:16:43 +0100

    refactor: simplified ValidationErrorType declaration

--- src/main/kotlin/uk/gov/justice/digital/hmpps/arnsriskactuarialapi/dto/ValidationErrorType.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/arnsriskactuarialapi/dto/ValidationErrorType.kt
@@ -1,24 +1,27 @@
 package uk.gov.justice.digital.hmpps.arnsriskactuarialapi.dto
 
-enum class ValidationErrorType(val asErrorResponse: (List<String>) -> ValidationErrorResponse) {
+enum class ValidationErrorType(val message: String) {
 
-  SEXUAL_OFFENDING_MISSING_COUNTS({ fields -> ValidationErrorResponse(SEXUAL_OFFENDING_MISSING_COUNTS, "Sexual motivation/offending identified - complete sexual offence counts", fields) }),
-  SEXUAL_OFFENDING_INCONSISTENT_INPUT({ fields -> ValidationErrorResponse(SEXUAL_OFFENDING_INCONSISTENT_INPUT, "No sexual motivation/offending identified - additional fields should not be provided", fields) }),
-  NEED_DETAILS_OF_EXACT_OFFENCE({ fields -> ValidationErrorResponse(NEED_DETAILS_OF_EXACT_OFFENCE, "For this group of offences, the OGRS 3 offence category takes different values depending on the nature of the exact offence. Therefore, it is not possible to calculate an OGRS 3 score without details of the exact offence.", fields) }),
-  DOMESTIC_ABUSE_INCONSISTENT_INPUT({ fields -> ValidationErrorResponse(DOMESTIC_ABUSE_INCONSISTENT_INPUT, "No evidence of domestic abuse identified - domesticAbuseAgainstPartner and domesticAbuseAgainstFamily should not be provided", fields) }),
-  MISSING_MANDATORY_INPUT({ fields -> ValidationErrorResponse(MISSING_MANDATORY_INPUT, "Mandatory input field(s) missing", fields) }),
-  AGE_AT_FIRST_SANCTION_AFTER_AGE_AT_CURRENT_CONVICTION({ fields -> ValidationErrorResponse(AGE_AT_FIRST_SANCTION_AFTER_AGE_AT_CURRENT_CONVICTION, "Age at first sanction must be before age at current conviction", fields) }),
-  TOTAL_NUMBER_OF_SANCTIONS_LESS_THAN_ONE({ fields -> ValidationErrorResponse(TOTAL_NUMBER_OF_SANCTIONS_LESS_THAN_ONE, "Total number of sanctions must be one or greater", fields) }),
-  AGE_AT_CURRENT_CONVICTION_LESS_THAN_TEN({ fields -> ValidationErrorResponse(AGE_AT_CURRENT_CONVICTION_LESS_THAN_TEN, "Age at current conviction must be 10 or greater", fields) }),
-  LDS_NOT_ENOUGH_FIELDS_PRESENT({ fields -> ValidationErrorResponse(LDS_NOT_ENOUGH_FIELDS_PRESENT, "At least three input fields must be provided", fields) }),
-  OFFENCE_CODE_INCORRECT_FORMAT({ fields -> ValidationErrorResponse(OFFENCE_CODE_INCORRECT_FORMAT, "Offence code must be a string of 5 digits", fields) }),
-  OFFENCE_CODE_MAPPING_NOT_FOUND({ fields -> ValidationErrorResponse(OFFENCE_CODE_MAPPING_NOT_FOUND, "No offence code to actuarial weighting mapping found for offence code", fields) }),
+  DOMESTIC_ABUSE_INCONSISTENT_INPUT("No evidence of domestic abuse identified - domesticAbuseAgainstPartner and domesticAbuseAgainstFamily should not be provided"),
+  SEXUAL_OFFENDING_MISSING_COUNTS("Sexual motivation/offending identified - complete sexual offence counts"),
+  SEXUAL_OFFENDING_INCONSISTENT_INPUT("No sexual motivation/offending identified - additional fields should not be provided"),
+  NEED_DETAILS_OF_EXACT_OFFENCE("For this group of offences, the OGRS 3 offence category takes different values depending on the nature of the exact offence. Therefore, it is not possible to calculate an OGRS 3 score without details of the exact offence."),
+  MISSING_MANDATORY_INPUT("Mandatory input field(s) missing"),
+  AGE_AT_FIRST_SANCTION_AFTER_AGE_AT_CURRENT_CONVICTION("Age at first sanction must be before age at current conviction"),
+  TOTAL_NUMBER_OF_SANCTIONS_LESS_THAN_ONE("Total number of sanctions must be one or greater"),
+  AGE_AT_CURRENT_CONVICTION_LESS_THAN_TEN("Age at current conviction must be 10 or greater"),
+  LDS_NOT_ENOUGH_FIELDS_PRESENT("At least three input fields must be provided"),
+  OFFENCE_CODE_INCORRECT_FORMAT("Offence code must be a string of 5 digits"),
+  OFFENCE_CODE_MAPPING_NOT_FOUND("No offence code to actuarial weighting mapping found for offence code"),
 
   // Potential Legacy
-  NOT_APPLICABLE({ fields -> ValidationErrorResponse(NOT_APPLICABLE, "ERR1 - Does not meet eligibility criteria", fields) }),
-  BELOW_MIN_VALUE({ fields -> ValidationErrorResponse(BELOW_MIN_VALUE, "ERR2 - Below minimum value", fields) }),
-  ABOVE_MAX_VALUE({ fields -> ValidationErrorResponse(ABOVE_MAX_VALUE, "ERR3 - Above minimum value", fields) }),
-  NO_MATCHING_INPUT({ fields -> ValidationErrorResponse(NO_MATCHING_INPUT, "ERR4 - Does not match agreed input", fields) }),
-  MISSING_INPUT({ fields -> ValidationErrorResponse(MISSING_INPUT, "ERR5 - Field is Null", fields) }),
-  UNEXPECTED_VALUE({ fields -> ValidationErrorResponse(UNEXPECTED_VALUE, "ERR6 - Field is unexpected", fields) }),
+  NOT_APPLICABLE("ERR1 - Does not meet eligibility criteria"),
+  BELOW_MIN_VALUE("ERR2 - Below minimum value"),
+  ABOVE_MAX_VALUE("ERR3 - Above minimum value"),
+  NO_MATCHING_INPUT("ERR4 - Does not match agreed input"),
+  MISSING_INPUT("ERR5 - Field is Null"),
+  UNEXPECTED_VALUE("ERR6 - Field is unexpected"),
+  ;
+
+  fun asErrorResponse(fields: List<String>): ValidationErrorResponse = ValidationErrorResponse(this, message, fields)
 }
