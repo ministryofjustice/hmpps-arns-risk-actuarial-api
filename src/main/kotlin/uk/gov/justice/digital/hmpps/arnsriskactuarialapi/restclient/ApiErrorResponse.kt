45c4721b41ef48a880fdd6f2a332cb3e7bca0b12
Thomas Bedford <thomas.bedford@hippodigital.co.uk>
2025-11-12 16:58:18 +0000

    Amended dev config for manage-offences-api and redis

--- src/main/kotlin/uk/gov/justice/digital/hmpps/arnsriskactuarialapi/restclient/ApiErrorResponse.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/arnsriskactuarialapi/restclient/ApiErrorResponse.kt
@@ -1,5 +1,8 @@
 package uk.gov.justice.digital.hmpps.arnsriskactuarialapi.restclient
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties
+
+@JsonIgnoreProperties(ignoreUnknown = true)
 data class ApiErrorResponse(
   val status: Int? = 0,
   val developerMessage: String? = null,
