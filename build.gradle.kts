caaddf4866f9bad970344cdf7a5b1760a34ee409
James Boobier <139986710+JamesBoobier@users.noreply.github.com>
2026-03-24 16:20:28 +0000

    Update dependencies to latest spring boot 3

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,7 @@ import kotlinx.kover.gradle.plugin.dsl.AggregationType
 import kotlinx.kover.gradle.plugin.dsl.CoverageUnit
 
 plugins {
-  id("uk.gov.justice.hmpps.gradle-spring-boot") version "9.3.0"
+  id("uk.gov.justice.hmpps.gradle-spring-boot") version "9.5.0"
   id("org.jetbrains.kotlinx.kover") version "0.9.1"
   kotlin("plugin.spring") version "2.1.21"
 }
@@ -26,23 +26,23 @@ kover {
 }
 
 dependencies {
-  implementation("uk.gov.justice.service.hmpps:hmpps-kotlin-spring-boot-starter:1.4.11")
+  implementation("uk.gov.justice.service.hmpps:hmpps-kotlin-spring-boot-starter:1.8.2")
   implementation("org.springframework.boot:spring-boot-starter-webflux")
   implementation("org.springframework.boot:spring-boot-starter-data-redis")
-  implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.8.9")
-  implementation("org.apache.commons:commons-csv:1.9.0")
+  implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.8.16")
+  implementation("org.apache.commons:commons-csv:1.14.1")
   implementation(kotlin("reflect"))
 
-  testImplementation("uk.gov.justice.service.hmpps:hmpps-kotlin-spring-boot-starter-test:1.4.11")
-  testImplementation("org.wiremock:wiremock-standalone:3.13.1")
-  testImplementation("io.swagger.parser.v3:swagger-parser:2.1.30") {
+  testImplementation("uk.gov.justice.service.hmpps:hmpps-kotlin-spring-boot-starter-test:1.8.2")
+  testImplementation("org.wiremock:wiremock-standalone:3.13.2")
+  testImplementation("io.swagger.parser.v3:swagger-parser:2.1.39") {
     exclude(group = "io.swagger.core.v3")
   }
-  testImplementation("org.junit.jupiter:junit-jupiter-params:5.11.4")
+  testImplementation("org.junit.jupiter:junit-jupiter-params")
   testImplementation(kotlin("test"))
   testImplementation("com.oracle.database.jdbc:ojdbc11:23.9.0.25.07")
   testImplementation("com.oracle.database.xml:xdb:23.9.0.25.07")
-  testImplementation(platform("org.testcontainers:testcontainers-bom:2.0.1"))
+  testImplementation(platform("org.testcontainers:testcontainers-bom:2.0.4"))
   testImplementation("org.testcontainers:testcontainers")
 }
 
