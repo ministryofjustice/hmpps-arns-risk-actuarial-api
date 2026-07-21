414ce2a7dbb54cd24a7b4a568a134b51e6ca8451
James Boobier <139986710+JamesBoobier@users.noreply.github.com>
2026-07-21 11:11:48 +0100

    ACT-625 Switch dev to use real manage offences api (#244)
    * ACT-625 Switch dev to use real manage offences api
    
    * revert unwanted changes
    
    * revert unwanted changes
    
    * fix tests

--- src/test/kotlin/uk/gov/justice/digital/hmpps/arnsriskactuarialapi/integration/controller/AdminControllerTest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/arnsriskactuarialapi/integration/controller/AdminControllerTest.kt
@@ -34,37 +34,37 @@ class AdminControllerTest : IntegrationTestBase() {
 
     val actualOffenceCodeMappings: Map<String, OffenceCodeDetails> = keys.associateWith { key -> redisTemplate.opsForValue().get(key)!! }
 
-    val offenceCodeValuesOffenceCodeValues00100 = actualOffenceCodeMappings.get("offence_code_mapping_00100")
+    val offenceCodeValuesOffenceCodeValues00100 = actualOffenceCodeMappings["offence_code_mapping_00100"]
     assertEquals("Violence against the person", offenceCodeValuesOffenceCodeValues00100?.parentGroupDescription)
     assertEquals("Murder", offenceCodeValuesOffenceCodeValues00100?.categoryDescription)
     assertEquals("Murder    [Use this code only if you are unable to determine which subcoded Offence applies]", offenceCodeValuesOffenceCodeValues00100?.subCategoryDescription)
     assertEquals(ActuarialCategory.VIOLENCE_AGAINST_THE_PERSON_ABH_PLUS, offenceCodeValuesOffenceCodeValues00100?.actuarialCategory)
     assertTrue(offenceCodeValuesOffenceCodeValues00100?.flags?.opdViolenceSex!!)
-    assertTrue(offenceCodeValuesOffenceCodeValues00100?.flags?.isViolentSanction!!)
+    assertTrue(offenceCodeValuesOffenceCodeValues00100.flags.isViolentSanction)
 
-    val offenceCodeValuesOffenceCodeValues08800 = actualOffenceCodeMappings.get("offence_code_mapping_08800")
+    val offenceCodeValuesOffenceCodeValues08800 = actualOffenceCodeMappings["offence_code_mapping_08800"]
     assertEquals("Sexual offences", offenceCodeValuesOffenceCodeValues08800?.parentGroupDescription)
     assertEquals("Miscellaneous sexual offences", offenceCodeValuesOffenceCodeValues08800?.categoryDescription)
     assertEquals("Miscellaneous sexual offences    [Use this code only if you are unable to determine which subcoded Offence applies]", offenceCodeValuesOffenceCodeValues08800?.subCategoryDescription)
-    assertEquals(ActuarialCategory.NEED_DETAILS_OF_EXACT_OFFENCE, offenceCodeValuesOffenceCodeValues08800?.actuarialCategory)
+    assertEquals(ActuarialCategory.SEXUAL_NOT_AGAINST_CHILD, offenceCodeValuesOffenceCodeValues08800?.actuarialCategory)
     assertTrue(offenceCodeValuesOffenceCodeValues08800?.flags?.opdViolenceSex!!)
-    assertFalse(offenceCodeValuesOffenceCodeValues08800?.flags?.isViolentSanction!!)
+    assertFalse(offenceCodeValuesOffenceCodeValues08800.flags.isViolentSanction)
 
-    val offenceCodeValuesOffenceCodeValues11618 = actualOffenceCodeMappings.get("offence_code_mapping_11618")
+    val offenceCodeValuesOffenceCodeValues11618 = actualOffenceCodeMappings["offence_code_mapping_11618"]
     assertEquals("Other summary offences", offenceCodeValuesOffenceCodeValues11618?.parentGroupDescription)
     assertEquals("Fishery Laws, offences against", offenceCodeValuesOffenceCodeValues11618?.categoryDescription)
     assertEquals("Offences against Conservation of Seals Act 1970", offenceCodeValuesOffenceCodeValues11618?.subCategoryDescription)
     assertEquals(ActuarialCategory.OTHER_OFFENCES, offenceCodeValuesOffenceCodeValues11618?.actuarialCategory)
     assertFalse(offenceCodeValuesOffenceCodeValues11618?.flags?.opdViolenceSex!!)
-    assertFalse(offenceCodeValuesOffenceCodeValues11618?.flags?.isViolentSanction!!)
+    assertFalse(offenceCodeValuesOffenceCodeValues11618.flags.isViolentSanction)
 
-    val offenceCodeValuesOffenceCodeValues99968 = actualOffenceCodeMappings.get("offence_code_mapping_99968")
+    val offenceCodeValuesOffenceCodeValues99968 = actualOffenceCodeMappings["offence_code_mapping_99968"]
     assertEquals("Other indictable", offenceCodeValuesOffenceCodeValues99968?.parentGroupDescription)
     assertEquals("Libel", offenceCodeValuesOffenceCodeValues99968?.categoryDescription)
     assertEquals("Libel", offenceCodeValuesOffenceCodeValues99968?.subCategoryDescription)
     assertEquals(ActuarialCategory.OTHER_OFFENCES, offenceCodeValuesOffenceCodeValues99968?.actuarialCategory)
     assertFalse(offenceCodeValuesOffenceCodeValues99968?.flags?.opdViolenceSex!!)
-    assertFalse(offenceCodeValuesOffenceCodeValues99968?.flags?.isViolentSanction!!)
+    assertFalse(offenceCodeValuesOffenceCodeValues99968.flags.isViolentSanction)
   }
 
   private fun clearOffenceCodeCache() {
